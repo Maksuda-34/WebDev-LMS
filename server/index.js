6726afe5cc2ddceee43fe0db552025fb7e9e13cc
Maksuda-34 <maksudaakter@iut-dhaka.edu>
2023-03-30 01:34:07 +0600

    Completed CRUD

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ app.get("/", (req, res) => {
     res.json("hello from server");
 })
 
+//get all books
 app.get("/books", (req, res) => {
     const q = "SELECT * FROM books";
     db.query(q, (err,data)=> {
@@ -28,6 +29,7 @@ app.get("/books", (req, res) => {
     }) 
 })
 
+//insert a book
 app.post("/books", (req, res) => {
     const q = "INSERT INTO books(`title`,`desc`,`copies`,`cover`,`status`) VALUES(?)"
     const values = [
@@ -44,6 +46,7 @@ app.post("/books", (req, res) => {
     });
 });
 
+//delete books
 app.delete("/books/:id", (req, res) => {
     const bookId = req.params.id;
     const q = " DELETE FROM books WHERE id = ? ";
@@ -53,6 +56,58 @@ app.delete("/books/:id", (req, res) => {
       return res.json(data);
     });
   });
+
+//update book
+app.put("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "UPDATE books SET `title`= ?, `desc`= ?, `copies`= ?, `cover`= ? ,`status`= ? WHERE id = ?";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.copies,
+      req.body.cover,
+      req.body.status
+    ];
+  
+    db.query(q, [...values,bookId], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  });
+
+  app.get("/books", (req, res) => {
+    const search = req.query.search;
+    const filter = req.query.filter;
+
+    let q = "SELECT * FROM books";
+
+    const params = [];
+
+    // Apply search filter
+    if (search) {
+        q += " WHERE title LIKE ? OR `desc` LIKE ?";
+        params.push(`%${search}%`);
+        params.push(`%${search}%`);
+    }
+
+    // Apply filter
+    if (filter) {
+        if (params.length === 0) {
+            q += " WHERE";
+        } else {
+            q += " AND";
+        }
+        q += " status = ?";
+        params.push(filter);
+    }
+
+    db.query(q, params, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    });
+});
+
 app.listen(5000, ()=>{
     console.log("Connected to the server");
 })
